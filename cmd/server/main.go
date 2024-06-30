cbd9749e3e1729280766a8d138262430bcdd4611
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-07-01 01:08:03 +0300

    Add server start to main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"github.com/kirillgashkov/assignment-youthumb/internal/api"
 	"github.com/kirillgashkov/assignment-youthumb/internal/config"
 	"github.com/kirillgashkov/assignment-youthumb/internal/logger"
+	"github.com/kirillgashkov/assignment-youthumb/proto/youthumbpb/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 	"log/slog"
+	"net"
 )
 
 func main() {
@@ -24,5 +29,19 @@ func mainErr() error {
 	}
 	slog.SetDefault(log)
 
-	return nil
+	srv := grpc.NewServer()
+	if cfg.Mode == config.ModeDevelopment {
+		reflection.Register(srv)
+	}
+	youthumbpb.RegisterThumbnailServiceServer(srv, &api.ThumbnailServiceServer{})
+
+	addr := &net.TCPAddr{IP: net.ParseIP(cfg.GRPC.Host), Port: cfg.GRPC.Port}
+	lis, err := net.ListenTCP("tcp", addr)
+	if err != nil {
+		return err
+	}
+
+	slog.Info("starting server", "addr", addr, "mode", cfg.Mode)
+	err = srv.Serve(lis)
+	return err
 }
