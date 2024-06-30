8e0e0a445f6def42e9ba1f3cc6e65dcf4d4387be
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-07-01 01:07:43 +0300

    Add ThumbnailServiceServer base

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,1 +1,15 @@
 package api
+
+import (
+	"github.com/kirillgashkov/assignment-youthumb/proto/youthumbpb/v1"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type ThumbnailServiceServer struct {
+	youthumbpb.UnimplementedThumbnailServiceServer
+}
+
+func (*ThumbnailServiceServer) GetThumbnail(*youthumbpb.GetThumbnailRequest, youthumbpb.ThumbnailService_GetThumbnailServer) error {
+	return status.Errorf(codes.Unimplemented, "method GetThumbnail not implemented")
+}
