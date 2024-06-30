a75faf741d9eec1cd9cfe8a65f0289f522c6a222
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-06-30 14:19:00 +0300

    Clean up logger

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -8,16 +8,16 @@ import (
 )
 
 func New(cfg *config.Config) (*slog.Logger, error) {
-	var l *slog.Logger
+	var log *slog.Logger
 
 	switch cfg.Mode {
 	case config.ModeDevelopment:
-		l = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
 	case config.ModeProduction:
-		l = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
 	default:
 		return nil, fmt.Errorf("invalid mode: %s", cfg.Mode)
 	}
 
-	return l, nil
+	return log, nil
 }
