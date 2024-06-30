aec2eeea40f2eaba2b4f1f6aa57c41bb5abb92bb
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-06-30 14:20:36 +0300

    Use config and logger in server main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,4 +1,28 @@
 package main
 
+import (
+	"github.com/kirillgashkov/assignment-youthumb/internal/config"
+	"github.com/kirillgashkov/assignment-youthumb/internal/logger"
+	"log/slog"
+)
+
 func main() {
+	if err := mainErr(); err != nil {
+		panic(err)
+	}
+}
+
+func mainErr() error {
+	cfg, err := config.New()
+	if err != nil {
+		return err
+	}
+
+	log, err := logger.New(cfg)
+	if err != nil {
+		return err
+	}
+	slog.SetDefault(log)
+
+	return nil
 }
