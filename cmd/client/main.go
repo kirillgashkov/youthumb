e493084e55d6a405a296f4b476f0b58e489d4dc1
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-07-01 21:26:49 +0300

    Add client main base

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,4 +1,30 @@
 package main
 
+import (
+	"github.com/kirillgashkov/assignment-youthumb/internal/config"
+	"github.com/kirillgashkov/assignment-youthumb/internal/logger"
+	"log/slog"
+	"os"
+)
+
 func main() {
+	if err := mainErr(); err != nil {
+		slog.Error("fatal", "error", err)
+		os.Exit(1)
+	}
+}
+
+func mainErr() error {
+	cfg, err := config.New()
+	if err != nil {
+		return err
+	}
+
+	log, err := logger.New(cfg)
+	if err != nil {
+		return err
+	}
+	slog.SetDefault(log)
+
+	return nil
 }
