736214316d19978a7b437f035db178056b37cff4
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-07-01 21:45:33 +0300

    Add client CLI

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,11 +1,32 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/kirillgashkov/assignment-youthumb/internal/config"
 	"github.com/kirillgashkov/assignment-youthumb/internal/logger"
 	"log/slog"
 	"os"
 )
+
+var (
+	isAsync   = flag.Bool("async", false, "Download thumbnails asynchronously.")
+	outputDir = flag.String("output", "", "Download thumbnails to the specified directory.")
+)
+
+func usage() {
+	fmt.Fprintf(flag.CommandLine.Output(), `Usage: %s [OPTIONS] VIDEO_URL...
+
+Download thumbnails for YouTube videos with the specified URLs.
+
+Arguments:
+  VIDEO_URL
+        The URL of the video to download the thumbnail for.
+
+Options:
+`, os.Args[0])
+	flag.PrintDefaults()
+}
 
 func main() {
 	if err := mainErr(); err != nil {
@@ -15,6 +36,9 @@ func main() {
 }
 
 func mainErr() error {
+	flag.Usage = usage
+	flag.Parse()
+
 	cfg, err := config.New()
 	if err != nil {
 		return err
