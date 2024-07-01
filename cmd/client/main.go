65af5d0349dae419c9aefd93f5abf08b4cb2a4a2
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-07-02 01:29:20 +0300

    Add thumbnail download base to client main

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"flag"
 	"fmt"
+	"github.com/kirillgashkov/assignment-youthumb/internal/api/client"
 	"github.com/kirillgashkov/assignment-youthumb/internal/config"
 	"github.com/kirillgashkov/assignment-youthumb/internal/logger"
+	"github.com/kirillgashkov/assignment-youthumb/proto/youthumbpb/v1"
 	"log/slog"
 	"os"
+	"sync"
+)
+
+const (
+	maxRetry = 3
+)
+
+var (
+	errUsage             = errors.New("usage")
+	errThumbnailNotFound = errors.New("thumbnail not found")
+	errCanceled          = errors.New("canceled")
 )
 
 var (
@@ -29,7 +44,12 @@ Options:
 }
 
 func main() {
-	if err := mainErr(); err != nil {
+	err := mainErr()
+	if errors.Is(err, errUsage) {
+		flag.Usage()
+		os.Exit(2)
+	}
+	if err != nil {
 		slog.Error("fatal", "error", err)
 		os.Exit(1)
 	}
@@ -38,6 +58,10 @@ func main() {
 func mainErr() error {
 	flag.Usage = usage
 	flag.Parse()
+
+	if flag.NArg() == 0 {
+		return errUsage
+	}
 
 	cfg, err := config.New()
 	if err != nil {
@@ -50,5 +74,63 @@ func mainErr() error {
 	}
 	slog.SetDefault(log)
 
+	clientConn, err := client.NewClientConn(cfg.GRPC)
+	if err != nil {
+		return err
+	}
+
+	cli, err := client.NewClient(clientConn)
+	if err != nil {
+		return err
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	wg := sync.WaitGroup{}
+	defer wg.Wait()
+
+	for _, videoURL := range flag.Args() {
+		if *isAsync {
+			wg.Add(1)
+			go func() { // https://go.dev/blog/loopvar-preview
+				defer wg.Done()
+				if err := downloadThumbnail(ctx, cli, videoURL, *outputDir); err != nil {
+					slog.Error("failed to download thumbnail", "video_url", videoURL, "error", err)
+				}
+			}()
+		} else {
+			if err := downloadThumbnail(ctx, cli, videoURL, *outputDir); err != nil {
+				slog.Error("failed to download thumbnail", "video_url", videoURL, "error", err)
+			}
+		}
+	}
+
+	return nil
+}
+
+func downloadThumbnail(ctx context.Context, cli youthumbpb.ThumbnailServiceClient, videoURL, outputDir string) error {
+	retry := 0
+	for {
+		if err := downloadThumbnailOnce(ctx, cli, videoURL, outputDir); err != nil {
+			if errors.Is(err, errCanceled) {
+				return err
+			}
+			if errors.Is(err, errThumbnailNotFound) {
+				return err
+			}
+			if retry == maxRetry {
+				return err
+			}
+			retry++
+			slog.Warn("retrying thumbnail download", "video_url", videoURL, "retry", retry, "error", err)
+			continue
+		}
+		return nil
+	}
+}
+
+func downloadThumbnailOnce(ctx context.Context, cli youthumbpb.ThumbnailServiceClient, videoURL, outputDir string) error {
+	slog.Info("downloading thumbnail", "video_url", videoURL)
 	return nil
 }
