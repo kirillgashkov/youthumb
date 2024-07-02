05c5fda9b49d8abe0ae9b6240b2309682d170c18
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-07-02 22:38:21 +0300

    Clean up youthumb.proto

--- proto/youthumbpb/v1/youthumb.pb.go
+++ proto/youthumbpb/v1/youthumb.pb.go
@@ -20,6 +20,55 @@ const (
 	_ = protoimpl.EnforceVersion(protoimpl.MaxVersion - 20)
 )
 
+// GetThumbnailRequest represents a request to get a thumbnail of a video.
+type GetThumbnailRequest struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	// video_url is a URL of the video for which a thumbnail should be sent.
+	VideoUrl string `protobuf:"bytes,1,opt,name=video_url,json=videoUrl,proto3" json:"video_url,omitempty"`
+}
+
+func (x *GetThumbnailRequest) Reset() {
+	*x = GetThumbnailRequest{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_youthumb_v1_youthumb_proto_msgTypes[0]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *GetThumbnailRequest) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*GetThumbnailRequest) ProtoMessage() {}
+
+func (x *GetThumbnailRequest) ProtoReflect() protoreflect.Message {
+	mi := &file_youthumb_v1_youthumb_proto_msgTypes[0]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use GetThumbnailRequest.ProtoReflect.Descriptor instead.
+func (*GetThumbnailRequest) Descriptor() ([]byte, []int) {
+	return file_youthumb_v1_youthumb_proto_rawDescGZIP(), []int{0}
+}
+
+func (x *GetThumbnailRequest) GetVideoUrl() string {
+	if x != nil {
+		return x.VideoUrl
+	}
+	return ""
+}
+
 // ThumbnailChunk represents a chunk of thumbnail data. The content type is
 // a MIME type of the data and is sent only once in the first message.
 type ThumbnailChunk struct {
@@ -36,7 +85,7 @@ type ThumbnailChunk struct {
 func (x *ThumbnailChunk) Reset() {
 	*x = ThumbnailChunk{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_youthumb_v1_youthumb_proto_msgTypes[0]
+		mi := &file_youthumb_v1_youthumb_proto_msgTypes[1]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -49,7 +98,7 @@ func (x *ThumbnailChunk) String() string {
 func (*ThumbnailChunk) ProtoMessage() {}
 
 func (x *ThumbnailChunk) ProtoReflect() protoreflect.Message {
-	mi := &file_youthumb_v1_youthumb_proto_msgTypes[0]
+	mi := &file_youthumb_v1_youthumb_proto_msgTypes[1]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -62,7 +111,7 @@ func (x *ThumbnailChunk) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use ThumbnailChunk.ProtoReflect.Descriptor instead.
 func (*ThumbnailChunk) Descriptor() ([]byte, []int) {
-	return file_youthumb_v1_youthumb_proto_rawDescGZIP(), []int{0}
+	return file_youthumb_v1_youthumb_proto_rawDescGZIP(), []int{1}
 }
 
 func (x *ThumbnailChunk) GetContentType() string {
@@ -79,69 +128,20 @@ func (x *ThumbnailChunk) GetData() []byte {
 	return nil
 }
 
-// GetThumbnailRequest represents a request to get a thumbnail of a video.
-type GetThumbnailRequest struct {
-	state         protoimpl.MessageState
-	sizeCache     protoimpl.SizeCache
-	unknownFields protoimpl.UnknownFields
-
-	// video_url is a URL of the video for which a thumbnail should be sent.
-	VideoUrl string `protobuf:"bytes,1,opt,name=video_url,json=videoUrl,proto3" json:"video_url,omitempty"`
-}
-
-func (x *GetThumbnailRequest) Reset() {
-	*x = GetThumbnailRequest{}
-	if protoimpl.UnsafeEnabled {
-		mi := &file_youthumb_v1_youthumb_proto_msgTypes[1]
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		ms.StoreMessageInfo(mi)
-	}
-}
-
-func (x *GetThumbnailRequest) String() string {
-	return protoimpl.X.MessageStringOf(x)
-}
-
-func (*GetThumbnailRequest) ProtoMessage() {}
-
-func (x *GetThumbnailRequest) ProtoReflect() protoreflect.Message {
-	mi := &file_youthumb_v1_youthumb_proto_msgTypes[1]
-	if protoimpl.UnsafeEnabled && x != nil {
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		if ms.LoadMessageInfo() == nil {
-			ms.StoreMessageInfo(mi)
-		}
-		return ms
-	}
-	return mi.MessageOf(x)
-}
-
-// Deprecated: Use GetThumbnailRequest.ProtoReflect.Descriptor instead.
-func (*GetThumbnailRequest) Descriptor() ([]byte, []int) {
-	return file_youthumb_v1_youthumb_proto_rawDescGZIP(), []int{1}
-}
-
-func (x *GetThumbnailRequest) GetVideoUrl() string {
-	if x != nil {
-		return x.VideoUrl
-	}
-	return ""
-}
-
 var File_youthumb_v1_youthumb_proto protoreflect.FileDescriptor
 
 var file_youthumb_v1_youthumb_proto_rawDesc = []byte{
 	0x0a, 0x1a, 0x79, 0x6f, 0x75, 0x74, 0x68, 0x75, 0x6d, 0x62, 0x2f, 0x76, 0x31, 0x2f, 0x79, 0x6f,
 	0x75, 0x74, 0x68, 0x75, 0x6d, 0x62, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x0b, 0x79, 0x6f,
-	0x75, 0x74, 0x68, 0x75, 0x6d, 0x62, 0x2e, 0x76, 0x31, 0x22, 0x47, 0x0a, 0x0e, 0x54, 0x68, 0x75,
-	0x6d, 0x62, 0x6e, 0x61, 0x69, 0x6c, 0x43, 0x68, 0x75, 0x6e, 0x6b, 0x12, 0x21, 0x0a, 0x0c, 0x63,
-	0x6f, 0x6e, 0x74, 0x65, 0x6e, 0x74, 0x5f, 0x74, 0x79, 0x70, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28,
-	0x09, 0x52, 0x0b, 0x63, 0x6f, 0x6e, 0x74, 0x65, 0x6e, 0x74, 0x54, 0x79, 0x70, 0x65, 0x12, 0x12,
-	0x0a, 0x04, 0x64, 0x61, 0x74, 0x61, 0x18, 0x02, 0x20, 0x01, 0x28, 0x0c, 0x52, 0x04, 0x64, 0x61,
-	0x74, 0x61, 0x22, 0x32, 0x0a, 0x13, 0x47, 0x65, 0x74, 0x54, 0x68, 0x75, 0x6d, 0x62, 0x6e, 0x61,
-	0x69, 0x6c, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12, 0x1b, 0x0a, 0x09, 0x76, 0x69, 0x64,
-	0x65, 0x6f, 0x5f, 0x75, 0x72, 0x6c, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x08, 0x76, 0x69,
-	0x64, 0x65, 0x6f, 0x55, 0x72, 0x6c, 0x32, 0x63, 0x0a, 0x10, 0x54, 0x68, 0x75, 0x6d, 0x62, 0x6e,
+	0x75, 0x74, 0x68, 0x75, 0x6d, 0x62, 0x2e, 0x76, 0x31, 0x22, 0x32, 0x0a, 0x13, 0x47, 0x65, 0x74,
+	0x54, 0x68, 0x75, 0x6d, 0x62, 0x6e, 0x61, 0x69, 0x6c, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74,
+	0x12, 0x1b, 0x0a, 0x09, 0x76, 0x69, 0x64, 0x65, 0x6f, 0x5f, 0x75, 0x72, 0x6c, 0x18, 0x01, 0x20,
+	0x01, 0x28, 0x09, 0x52, 0x08, 0x76, 0x69, 0x64, 0x65, 0x6f, 0x55, 0x72, 0x6c, 0x22, 0x47, 0x0a,
+	0x0e, 0x54, 0x68, 0x75, 0x6d, 0x62, 0x6e, 0x61, 0x69, 0x6c, 0x43, 0x68, 0x75, 0x6e, 0x6b, 0x12,
+	0x21, 0x0a, 0x0c, 0x63, 0x6f, 0x6e, 0x74, 0x65, 0x6e, 0x74, 0x5f, 0x74, 0x79, 0x70, 0x65, 0x18,
+	0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x0b, 0x63, 0x6f, 0x6e, 0x74, 0x65, 0x6e, 0x74, 0x54, 0x79,
+	0x70, 0x65, 0x12, 0x12, 0x0a, 0x04, 0x64, 0x61, 0x74, 0x61, 0x18, 0x02, 0x20, 0x01, 0x28, 0x0c,
+	0x52, 0x04, 0x64, 0x61, 0x74, 0x61, 0x32, 0x63, 0x0a, 0x10, 0x54, 0x68, 0x75, 0x6d, 0x62, 0x6e,
 	0x61, 0x69, 0x6c, 0x53, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x12, 0x4f, 0x0a, 0x0c, 0x47, 0x65,
 	0x74, 0x54, 0x68, 0x75, 0x6d, 0x62, 0x6e, 0x61, 0x69, 0x6c, 0x12, 0x20, 0x2e, 0x79, 0x6f, 0x75,
 	0x74, 0x68, 0x75, 0x6d, 0x62, 0x2e, 0x76, 0x31, 0x2e, 0x47, 0x65, 0x74, 0x54, 0x68, 0x75, 0x6d,
@@ -170,12 +170,12 @@ func file_youthumb_v1_youthumb_proto_rawDescGZIP() []byte {
 
 var file_youthumb_v1_youthumb_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
 var file_youthumb_v1_youthumb_proto_goTypes = []any{
-	(*ThumbnailChunk)(nil),      // 0: youthumb.v1.ThumbnailChunk
-	(*GetThumbnailRequest)(nil), // 1: youthumb.v1.GetThumbnailRequest
+	(*GetThumbnailRequest)(nil), // 0: youthumb.v1.GetThumbnailRequest
+	(*ThumbnailChunk)(nil),      // 1: youthumb.v1.ThumbnailChunk
 }
 var file_youthumb_v1_youthumb_proto_depIdxs = []int32{
-	1, // 0: youthumb.v1.ThumbnailService.GetThumbnail:input_type -> youthumb.v1.GetThumbnailRequest
-	0, // 1: youthumb.v1.ThumbnailService.GetThumbnail:output_type -> youthumb.v1.ThumbnailChunk
+	0, // 0: youthumb.v1.ThumbnailService.GetThumbnail:input_type -> youthumb.v1.GetThumbnailRequest
+	1, // 1: youthumb.v1.ThumbnailService.GetThumbnail:output_type -> youthumb.v1.ThumbnailChunk
 	1, // [1:2] is the sub-list for method output_type
 	0, // [0:1] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
@@ -190,7 +190,7 @@ func file_youthumb_v1_youthumb_proto_init() {
 	}
 	if !protoimpl.UnsafeEnabled {
 		file_youthumb_v1_youthumb_proto_msgTypes[0].Exporter = func(v any, i int) any {
-			switch v := v.(*ThumbnailChunk); i {
+			switch v := v.(*GetThumbnailRequest); i {
 			case 0:
 				return &v.state
 			case 1:
@@ -202,7 +202,7 @@ func file_youthumb_v1_youthumb_proto_init() {
 			}
 		}
 		file_youthumb_v1_youthumb_proto_msgTypes[1].Exporter = func(v any, i int) any {
-			switch v := v.(*GetThumbnailRequest); i {
+			switch v := v.(*ThumbnailChunk); i {
 			case 0:
 				return &v.state
 			case 1:
