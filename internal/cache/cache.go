5f3f3dd2fb59e206e8c884384dc3b621016a5f9f
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-07-02 05:42:57 +0300

    Implement cache

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -3,8 +3,11 @@
 package cache
 
 import (
-	_ "github.com/mattn/go-sqlite3"
+	"database/sql"
+	"errors"
 	"time"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 // Thumbnail represents a thumbnail image.
@@ -17,20 +20,52 @@ type Thumbnail struct {
 type Cache struct {
 	// Path to the SQLite database file.
 	Path string
+	db   *sql.DB
 }
 
 // New creates a new Cache with the given path to the SQLite database file.
-func New(path string) *Cache {
-	return &Cache{Path: path}
+func New(path string) (*Cache, error) {
+	db, err := sql.Open("sqlite3", path)
+	if err != nil {
+		return nil, err
+	}
+
+	createTableQuery := `
+		CREATE TABLE IF NOT EXISTS cache (
+			video_id TEXT PRIMARY KEY,
+			content_type TEXT NOT NULL,
+			data BLOB NOT NULL,
+			expires_at INTEGER NOT NULL
+		)
+	`
+	if _, err := db.Exec(createTableQuery); err != nil {
+		return nil, err
+	}
+
+	return &Cache{Path: path, db: db, getStmt: getStmt, setStmt: setStmt}, nil
 }
 
 // Get returns the thumbnail for the given video ID from the cache.
-func (c *Cache) Get(videoID string) ([]bool, error) {
-	return nil, nil
+func (c *Cache) Get(videoID string) (*Thumbnail, error) {
+	query := `SELECT content_type, data FROM cache WHERE video_id = ? AND expires_at > ?`
+
+	var contentType string
+	var data []byte
+	err := c.db.QueryRow(query, videoID, time.Now().Unix()).Scan(&contentType, &data)
+	if errors.Is(err, sql.ErrNoRows) {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	return &Thumbnail{ContentType: contentType, Data: data}, nil
 }
 
 // Set sets the thumbnail for the given video ID in the cache with the given
 // expiration time.
-func (c *Cache) Set(videoID string, thumbnail *Thumbnail, expireTime time.Time) error {
-	return nil
+func (c *Cache) Set(videoID string, thumbnail *Thumbnail, expiration time.Time) error {
+	query := `INSERT OR REPLACE INTO cache (video_id, content_type, data, expires_at) VALUES (?, ?, ?, ?)`
+	_, err := c.db.Exec(query, videoID, thumbnail.ContentType, thumbnail.Data, expiration.Unix())
+	return err
 }
