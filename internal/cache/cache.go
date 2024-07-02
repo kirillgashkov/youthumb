cb7df3843182fe05d6e7d9b547f4db27c37f63d0
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-07-02 04:58:06 +0300

    Add cache base

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -1,1 +1,36 @@
+// Package cache provides a key-value cache for thumbnails with expiration.
+// The cache is backed by an SQLite database.
 package cache
+
+import (
+	_ "github.com/mattn/go-sqlite3"
+	"time"
+)
+
+// Thumbnail represents a thumbnail image.
+type Thumbnail struct {
+	ContentType string
+	Data        []byte
+}
+
+// Cache is a key-value cache for thumbnails with expiration.
+type Cache struct {
+	// Path to the SQLite database file.
+	Path string
+}
+
+// New creates a new Cache with the given path to the SQLite database file.
+func New(path string) *Cache {
+	return &Cache{Path: path}
+}
+
+// Get returns the thumbnail for the given video ID from the cache.
+func (c *Cache) Get(videoID string) ([]bool, error) {
+	return nil, nil
+}
+
+// Set sets the thumbnail for the given video ID in the cache with the given
+// expiration time.
+func (c *Cache) Set(videoID string, thumbnail *Thumbnail, expireTime time.Time) error {
+	return nil
+}
