d00f299efbc961b3a22be9b4ad06c15c4cfb7955
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-07-02 16:56:23 +0300

    Add tools/golangci-lint stub

---
+++ tools/golangci-lint/main.go
@@ -0,0 +1,12 @@
+// golangci-lint is a wrapper around golangci-lint that downloads the binary and
+// runs it with the given arguments.
+//
+// It is not implemented yet but could be used in the future to avoid
+// dependencies on the host system. Its implementation could be similar to
+// tools/protoc's implementation but since golangci-lint is run way more often
+// than protoc, the binary should be cached.
+package main
+
+func main() {
+	panic("not implemented")
+}
